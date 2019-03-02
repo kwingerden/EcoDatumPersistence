d7eebc028e01516f7d673c059d53c8d7d2082d60
Ken Wingerden <ken.wingerden@gmail.com>
2019-03-02 13:51:41 -0800

    initial check in

---
+++ EcoDatumPersistence/EcoDatumPersistence.h
@@ -0,0 +1,19 @@
+//
+//  EcoDatumPersistence.h
+//  EcoDatumPersistence
+//
+//  Created by Kenneth Wingerden on 3/2/19.
+//  Copyright © 2019 Kenneth Wingerden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EcoDatumPersistence.
+FOUNDATION_EXPORT double EcoDatumPersistenceVersionNumber;
+
+//! Project version string for EcoDatumPersistence.
+FOUNDATION_EXPORT const unsigned char EcoDatumPersistenceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EcoDatumPersistence/PublicHeader.h>
+
+
