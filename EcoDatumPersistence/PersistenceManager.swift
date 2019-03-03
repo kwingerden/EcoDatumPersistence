75d0834da5f638e77e82f0432e973255ecc20c18
Ken Wingerden <ken.wingerden@gmail.com>
2019-03-02 16:26:51 -0800

    Added more tests

---
+++ EcoDatumPersistence/PersistenceManager.swift
@@ -0,0 +1,9 @@
+//
+//  PersistenceManager.swift
+//  EcoDatumPersistence
+//
+//  Created by Kenneth Wingerden on 3/2/19.
+//  Copyright © 2019 Kenneth Wingerden. All rights reserved.
+//
+
+import Foundation
