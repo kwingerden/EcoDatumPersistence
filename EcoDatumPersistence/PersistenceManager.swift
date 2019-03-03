81900ebe814356ead5c7d9351164c3f530ca902e
Ken Wingerden <ken.wingerden@gmail.com>
2019-03-02 16:28:51 -0800

    added tests

--- EcoDatumPersistence/PersistenceManager.swift
+++ EcoDatumPersistence/PersistenceManager.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+import EcoDatumCommon
+import EcoDatumModel
+import EcoDatumCoreData
+
+public class PeristenceManager {
+    
+    enum PeristenceError: Error {
+        case SiteNotFound
+        case InvalidSiteLocation
+    }
+    
+    public static let shared = PeristenceManager()
+    
+    private lazy var cdm: CoreDataManager = {
+        return CoreDataManager.shared
+    }()
+    
+    private init() {
+        
+    }
+    
+    public func new(site: Site) throws {
+        
+        try cdm.newSite(name: site.name)
+        
+    }
+    
+    public func update(site: Site) throws {
+        
+    }
+    
+    public func add(new ecoDatum: EcoDatum, to site: Site) throws {
+        guard let site = try cdm.getSite(byId: site.id) else {
+            throws PeristenceError.SiteNotFound
+        }
+        try cdm.newEcoDatum(site: )
+        try cdm.save()
+    }
+    
+}
